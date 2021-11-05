3701c5477a3f0daa8e6e3be6dc484bf995465d6a
Nikita Zerniukov <n.zernyukov@artsofte.ru>
2021-11-05 17:54:07 +0500

    123s

--- specs/task-4.spec.ts
+++ specs/task-4.spec.ts
@@ -1,14 +1,21 @@
 import { Vault } from "../src/task_3";
 import { ContractState, SmartContract } from "../src/task_4";
+import { Ruble } from "../src/task_2";
 
 
 test("Smart", () =>{
     const v1 = new Vault()
+    const v2 = new Vault()
+    v1.deposit(new Ruble(24))
+
     const smart = new SmartContract()
     expect(
          smart.state
     ).toBe(ContractState.pending);
 
+    smart.sender = {id: v1.id};
+    smart.receiver = {id: v2.id}
+    smart.value = new Ruble(20)
     smart.signAndTransfer();
 
     expect(smart.state).toBe(ContractState.transfer)
